0dbd5af743d61e148978ed0a365921599cf0393c
Risto Laurikainen <risto.laurikainen@csc.fi>
2013-11-28 14:53:43 +0200

    Added kmod-drbd84 to required packages on RedHat.

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,7 @@ class drbd::params {
       $package_name = 'drbd8-utils'
     }
     'RedHat': {
-      $package_name = 'drbd84-utils'
+      $package_name = [ 'drbd84-utils', 'kmod-drbd84' ]
     }
     default: {
       fail("The DRBD module is not supported on an ${::osfamily} based system.")
